bc23a8d80e7cfbd92234d9ab8264a82ebee0f02e
cur1ousFranz <xfranjeff@gmail.com>
2022-11-23 00:54:38 +0800

    change backend api domain

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import store from './store'
 
-// http://nearprinter-env-1.eba-zympgiwr.us-east-1.elasticbeanstalk.com
+// http://nearprinter-env.eba-adcjsuds.ap-southeast-1.elasticbeanstalk.com
 const axiosClient = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
 })
