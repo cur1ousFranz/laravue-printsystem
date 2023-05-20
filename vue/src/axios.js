99216ab0c33ce57cf0c9b9fc6ed410188462e9e5
cur1ousFranz <xfranjeff@gmail.com>
2023-05-20 22:26:13 +0800

    remove comments/whitespaces

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 import store from './store'
 
-// http://nearprinter-env.eba-adcjsuds.ap-southeast-1.elasticbeanstalk.com
-// http://127.0.0.1:8000/api
-// ${import.meta.env.VITE_API_BASE_URL}/api
 const axiosClient = axios.create({
   baseURL: `http://127.0.0.1:8000/api`
 })
